ecccaa824daeab242aa23c636f325bafa8465b77
Frank Mousset <moussetfrank@gmail.com>
2023-12-14 17:59:13 +0100

    Day 12

---
+++ aoc12/Main.hs
@@ -0,0 +1,52 @@
+import Data.List (intercalate)
+import Data.Map ((!))
+import Data.Map qualified as M
+import Data.Text qualified as T
+import Data.Text.IO qualified as T
+import System.Environment (getArgs)
+
+arra ss ns = fst $ arraMemo mempty (ss ++ ".") ns
+  where
+    arraMemo m ss ns =
+      let key = (length ss, length ns)
+       in if key `M.member` m
+            then (m ! key, m)
+            else (let (v, m') = arra' m ss ns in (v, M.insert key v m'))
+    arra' m ss [] = (if '#' `notElem` ss then 1 else 0, m)
+    arra' m [] ns = (0, m)
+    arra' m (s : ss) (n : ns)
+      | s == '#' && run (n - 1) `prefixOf` ss = arraMemo m (drop n ss) ns
+      | s == '.' = arraMemo m ss (n : ns)
+      | s == '?' =
+          let (a, m') = arraMemo m ss (n : ns)
+              (b, m'') = if run (n - 1) `prefixOf` ss then arraMemo m' (drop n ss) ns else (0, m')
+           in (a + b, m'')
+      | otherwise = (0, m)
+    run n = replicate n '#' ++ "."
+
+[] `prefixOf` _ = True
+(x : xs) `prefixOf` [] = False
+(x : xs) `prefixOf` (s : ss)
+  | s == x || s == '?' = xs `prefixOf` ss
+  | otherwise = False
+
+parse :: T.Text -> (String, [Int])
+parse line = let [a, b] = T.split (== ' ') line in (T.unpack a, read . T.unpack <$> T.split (== ',') b)
+
+part1 :: IO ()
+part1 = do
+  ls <- T.lines <$> T.getContents
+  print $ sum $ uncurry arra . parse <$> ls
+
+part2 :: IO ()
+part2 = do
+  ls <- T.lines <$> T.getContents
+  print $ sum $ uncurry arra . expand . parse <$> ls
+  where
+    expand (a, b) = (intercalate "?" $ replicate 5 a, concat $ replicate 5 b)
+
+main = getArgs >>= run
+  where
+    run ["part1"] = part1
+    run ["part2"] = part2
+    run _ = error "Missing argument"
